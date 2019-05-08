abdd7178c0aa52ed75a02f0fc12d454ccfee020a
salah <slhbhn@hotmail.com>
2019-05-08 11:20:29 +0100

    Configure ORB-SLAM2 to use opencv not opencv3

--- package/orb-slam2/orb-slam2.mk
+++ package/orb-slam2/orb-slam2.mk
@@ -6,7 +6,7 @@
 
 ORB_SLAM2_VERSION = c68f876a15a4d52de348096e3937ed8a71a4e39f
 ORB_SLAM2_SITE = $(call github,souilehbhn,ORB_SLAM2,$(ORB_SLAM2_VERSION))
-ORB_SLAM2_DEPENDENCIES = opencv3 pangolin eigen
+ORB_SLAM2_DEPENDENCIES = opencv pangolin eigen
 ORB_SLAM2_CONF_OPTS += -DCMAKE_CXX_FLAGS="$(TARGET_CXXFLAGS) -std=c++11"
 
 $(eval $(cmake-package))
