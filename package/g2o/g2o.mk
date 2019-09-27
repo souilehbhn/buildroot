5b502676ba8ca28e7f150bf5db7fe2cf28c03e4f
salah <slhbhn@hotmail.com>
2019-09-27 18:06:09 +0100

    add g2o, suitesparse, and lsd_slam

--- package/g2o/g2o.mk
+++ package/g2o/g2o.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-G2O_VERSION = 20170730_git
+G2O_VERSION = 9b41a4ea5ade8e1250b9c1b279f3a9c098811b5a  #20170730_git
 G2O_SITE = $(call github,RainerKuemmerle,g2o,$(G2O_VERSION))
 G2O_INSTALL_STAGING = YES
-G2O_DEPENDENCIES = eigen
+G2O_DEPENDENCIES = eigen suitesparse
 G2O_CONF_OPTS += -DARCH="arm"
 $(eval $(cmake-package))
